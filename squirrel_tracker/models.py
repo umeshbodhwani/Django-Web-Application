544776229ebb3436e2b43edaa6cfd8b1ac4f7d18
Umesh Bodhwani <umeshroxcent@gmail.com>
2019-12-03 20:32:23 +0000

    my first change to models.py file

--- squirrel_tracker/models.py
+++ squirrel_tracker/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from django.utils.translation import gettext as _
 # Create your models here.
+class squirrel(models.Model):
+    X = models.DecimalField(
+            help_text = _('Longitude')
+            )
