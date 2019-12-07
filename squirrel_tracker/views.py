5edc30634366edd527b0c3efb41a57e216e23413
Varun Challa <sc4597@columbia.edu>
2019-12-07 01:42:06 +0000

    Created Views

--- squirrel_tracker/views.py
+++ squirrel_tracker/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, redirect
+
+from django.http import HttpResponse
+
+
+#def index(request):
+#    return HttpResponse("Hello, world. You're at the squirrel tracker  index.")
+
+def get_map(request):
+        sight = Squirrel.objects.all()[:50]
+            context ={
+                                'sightings' :sight,
+                                }
+                return render(request, 'squirrel_tracker/map.html',context)
